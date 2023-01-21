5917e786d10a852f9457f777bb073512db03cab8
Naveed8625 <123239788+Naveed8625@users.noreply.github.com>
2023-01-21 16:55:16 +0530

    Update pract.js
    pract

--- pract.js
+++ pract.js
@@ -1,6 +1,5 @@
 function app(){
-    return React.createElement("p",{id:"p-tag"},"my name is naveed ali")
+    return React.createElement("p",{id:"p-tag"},"my name is naveed ali from nanded")
 }
 
 ReactDom.createRoot(document.getElementById("total")).render(React.createElement(app));
-//ReactDom.createRoot(document.getElementById("all")).render(React.createElement(app));
